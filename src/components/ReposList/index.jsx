dcbda38ef32d9c4f628faeab1915d268d7ea2aea
Fabio <fabiofreitascmav@gmail.com>
2024-10-16 20:25:50 -0300

    tratando erros

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,46 +1,56 @@
 import { useEffect, useState } from "react";
-
 import styles from './ReposList.module.css';
 
-const ReposList = ({ nomeUsuario}) => {
+const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaEstaCarregando, setEstaCarregando] = useState(true);
+    const [falhaErro, setFalhaErro] = useState(false);
 
     useEffect(() => {
         setEstaCarregando(true);
-        fetch(`https://api.github.com/users/${ nomeUsuario }/repos`)
-        .then(res => res.json())
-        .then(resJson => {
-            setTimeout(() => {
+        setFalhaErro(false);
+        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Erro ao buscar repositórios');
+                }
+                return res.json();
+            })
+            .then(resJson => {
+                setTimeout(() => {
+                    setEstaCarregando(false);
+                    setRepos(resJson);
+                }, 3000);
+            })
+            .catch(e => {
                 setEstaCarregando(false);
-                setRepos(resJson);
-            }, 3000)
-        })
+                setFalhaErro(true);
+            });
     }, [nomeUsuario]);
 
     return (
         <div className="container">
             {estaEstaCarregando ? (
                 <h1>Carregando...</h1>
+            ) : falhaErro ? (
+                <h1>Erro ao carregar os repositórios</h1>
             ) : (
                 <ul className={styles.list}>
-                    {repos.map(({id, name, language, html_url}) => (
+                    {repos.map(({ id, name, language, html_url }) => (
                         <li className={styles.listItem} key={id}>
                             <div className={styles.itemName}>
-                            <b>Nome:</b> 
-                            {name}
+                                <b>Nome:</b> {name}
                             </div>
                             <div className={styles.itemLanguage}>
-                            <b>linguagem:</b>  
-                            {language}
+                                <b>linguagem:</b> {language}
                             </div>
                             <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no Github</a>
                         </li>
                     ))}
-                </ul>        
+                </ul>
             )}
-        </ div>
-    )
-}
+        </div>
+    );
+};
 
 export default ReposList;
